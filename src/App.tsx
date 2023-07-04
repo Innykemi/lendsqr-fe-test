7e0f9dabd67f8a960be2c689d4c880cb2fe37758
Oluwakemi Raimi <oluwakemiraimi.nig@gmail.com>
2023-07-04 03:12:43 +0100

    project initialization

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Logo from "./assets/images/logo.svg";
+
+function App() {
+  return (
+    <div className="">
+        <img src={Logo} alt="Lendsqr" />
+        <h1>Welcome!</h1>
+    </div>
+  );
+}
+
+export default App;
