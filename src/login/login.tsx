22a5c875c6c586da6989b15c98ea7940b62e7e78
Oluwakemi Raimi <oluwakemiraimi.nig@gmail.com>
2023-07-06 00:03:40 +0100

    update icon

--- src/login/login.tsx
+++ src/login/login.tsx
@@ -1,15 +1,15 @@
-import Logo from "../assets/images/logo.svg";
-import LoginImage from "../assets/images/login-img.svg";
+import { ReactComponent as Logo } from "../assets/images/logo.svg";
+import { ReactComponent as LoginImage } from "../assets/images/login-img.svg";
 
 export default function Login() {
   return (
     <div className="login-wrapper">
       <div className="login-wrapper__logo-banner">
-        <img src={Logo} className="logo-img" alt="Lendsqr" />
+        <Logo className="logo-img" />
       </div>
 
       <div className="login-wrapper__login-img-wrapper">
-        <img src={LoginImage} className="login-img" alt="Lendsqr" />
+        <LoginImage className="login-img" />
       </div>
 
       <div className="login-wrapper__form-wrapper">
