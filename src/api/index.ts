11fd577670b092c749c8b73fbd2ce60c4a4a5279
Oluwakemi Raimi <oluwakemiraimi.nig@gmail.com>
2023-07-08 07:51:07 +0100

    cleanup

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,6 @@ export const getAllUsers = async () => {
   try {
     const response = await fetch('https://run.mocky.io/v3/f69883b1-d5f3-4b9a-a877-27df73449bd2');
     const data = await response.json();
-    console.log(data);
     return data;
   } catch (error) {
     console.error('Error fetching users:', error);
